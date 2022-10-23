b52d2021f5dc3f345cc1a3b060468552f6a051dd
Stephen Bailey <stkbailey@gmail.com>
2022-10-22 21:07:37 -0400

    format

--- dagster_jaffle_shop/utils/resources.py
+++ dagster_jaffle_shop/utils/resources.py
@@ -7,12 +7,14 @@ from dagster import resource, get_dagster_logger, InitResourceContext
 DUCKDB_FILE = "/tmp/duckdb/dagster.duckdb"
 logger = get_dagster_logger()
 
+
 class DuckDBResource:
     """
     Utility class for executing queries against a DuckDB database.
     This is mainly useful so that we know connections are made within
     the operations actually executed.
     """
+
     def __init__(self, database: str):
         self.database = database
 
@@ -20,7 +22,7 @@ class DuckDBResource:
         with duckdb.connect(database=self.database, read_only=read_only) as conn:
             logger.info("Executing query: %s", query)
             df = conn.execute(query).fetch_df()
-        
+
         if not "df" in locals():
             raise Exception("There was an error with the query!")
 
@@ -31,5 +33,5 @@ class DuckDBResource:
 def duckdb_resource(context: InitResourceContext) -> duckdb.DuckDBPyConnection:
     "The DuckDB resource yields a read-only connection to a local database."
 
-    db_file = pathlib.Path(DUCKDB_FILE)  
+    db_file = pathlib.Path(DUCKDB_FILE)
     yield DuckDBResource(database=db_file.as_posix())
