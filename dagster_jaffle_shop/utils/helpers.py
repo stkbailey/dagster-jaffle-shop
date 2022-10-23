430eade3a2802b7aff43b42f9b32a0b4d42c78b7
Stephen Bailey <stkbailey@gmail.com>
2022-10-22 21:12:00 -0400

    Update helpers

--- dagster_jaffle_shop/utils/helpers.py
+++ dagster_jaffle_shop/utils/helpers.py
@@ -9,18 +9,19 @@ def resolve_duckdb_ref(name):
     return name
 
 
-def render_jinja_template(q: str):
+def render_jinja_template(q: str) -> str:
     template = jinja2.Template(q)
     result = template.render(ref=resolve_duckdb_ref)
     return result
 
 
-def get_seed_filepath(name):
+def get_seed_filepath(name) -> str:
+    "Get the absolute path to a seed file"
     root_dir = pathlib.Path(__file__).parent.parent
     f = root_dir / "include" / name
     return f.absolute().as_posix()
 
 
-def mute_dagster_warnings():
+def mute_dagster_warnings() -> None:
     "Filter extraneous warnings that Dagster emits"
     warnings.filterwarnings("ignore", category=ExperimentalWarning)
